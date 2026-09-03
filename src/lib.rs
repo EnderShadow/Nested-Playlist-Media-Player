80da8a10b0b33e28c035cd783f1614c6ff8e94e2
Matthew Warren <professional@matthewwarren.me>
2026-09-03 19:06:52 -0400

    Implement loading config and library with TODOs for exceptional situations when loading

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::num::NonZeroU32;
+use std::path::Path;
 use std::time::Duration;
 use uuid::Uuid;
 use serde::{Serialize, Deserialize, Deserializer};
@@ -12,13 +16,20 @@ pub struct AudioSource {
     #[serde(alias = "uri")]
     pub path: String,
     pub title: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub artist: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub album: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub genre: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub album_artist: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub track_number: Option<u32>,
     #[serde(deserialize_with = "ok_or_default")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub track_count: Option<NonZeroU32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub year: Option<String>,
     #[serde(with = "serde_millis")]
     pub duration: Duration,
@@ -33,7 +44,7 @@ pub struct Playlist {
     pub contents: Vec<PlaylistEntry>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Copy, Clone)]
 #[serde(tag = "type", content = "uuid", rename_all = "lowercase")]
 pub enum PlaylistEntry {
     Song(Uuid),
@@ -52,11 +63,124 @@ pub struct MediaLibrary {
 }
 
 impl MediaLibrary {
-    // TODO take config as a parameter
-    pub fn load() -> MediaLibrary {
+    fn new() -> Self {
         MediaLibrary {
             songs: HashMap::new(),
             playlists: HashMap::new()
         }
     }
+
+    pub fn load(config: Config) -> Self {
+        let mut library = Self::new();
+
+        let data_dir = Path::new(&config.data_directory);
+        if !data_dir.exists() {
+            fs::create_dir_all(data_dir).expect("Could not create data directory");
+        }
+
+        let playlist_dir = data_dir.join("playlists");
+        if !playlist_dir.exists() {
+            fs::create_dir_all(&playlist_dir).expect("Could not create playlist directory");
+        }
+
+        let library_file = data_dir.join("library.json");
+        if !library_file.exists() {
+            let mut file = File::create_new(&library_file).expect("Could not create library file");
+            let _temp_vec: Vec<AudioSource> = Vec::new();
+            let default_data = serde_json::to_string(&_temp_vec).expect("Could not serialize empty vec");
+            let result = file.write_all(default_data.as_bytes());
+            if result.is_err() {
+                fs::remove_file(library_file).expect("Could not remove empty library file");
+                panic!("Could not write default library to file");
+            }
+        }
+
+        library.load_songs(library_file);
+        library.load_playlists(playlist_dir);
+
+        library
+    }
+
+    fn load_songs(&mut self, library_file: impl AsRef<Path>) {
+        // the library file should be accessible, so this `expect` should never fail
+        let library_file = File::open(library_file).expect("Could not open library file");
+        let songs: Vec<AudioSource> = serde_json::from_reader(library_file).expect("Failed to parse library");
+        songs.into_iter().for_each(|song| {self.songs.insert(song.uuid, song);});
+    }
+
+    fn load_playlists(&mut self, playlist_dir: impl AsRef<Path>) {
+        let playlists: Vec<_> = playlist_dir.as_ref().read_dir().expect("Could not read playlist directory").filter_map(|playlist_file| {
+            let file = playlist_file.ok()?;
+            let file = File::open(file.path()).ok()?;
+            let playlist: Option<Playlist> = serde_json::from_reader(file).ok();
+            playlist
+        }).collect();
+
+        playlists.into_iter().for_each(|playlist| {self.playlists.insert(playlist.uuid, playlist);});
+
+        for (_, playlist) in &self.playlists {
+            if self.check_contains_loop(&vec![], playlist) {
+                todo!("A playlist loop has been detected, implement error handling for this")
+            }
+
+            if self.contains_missing_playlist(playlist) {
+                todo!("A playlist references a non-existent playlist")
+            }
+
+            if self.contains_missing_song(playlist) {
+                todo!("A playlist references a non-existent song")
+            }
+        }
+    }
+
+    fn check_contains_loop(&self, seen_uuids: &Vec<Uuid>, playlist: &Playlist) -> bool {
+        if seen_uuids.contains(&playlist.uuid) {
+            true
+        } else {
+            let mut seen_uuids = seen_uuids.clone();
+            seen_uuids.push(playlist.uuid);
+            playlist.contents.iter().any(|entry| {
+                if let PlaylistEntry::Playlist(uuid) = entry {
+                    let playlist = self.playlists.get(uuid);
+                    if let Some(playlist) = playlist {
+                        self.check_contains_loop(&seen_uuids, playlist)
+                    } else {
+                        // playlist references a non-existent playlist, this will be handled after loops are checked
+                        false
+                    }
+                } else {
+                    // Don't need to check a playlist against a song
+                    false
+                }
+            })
+        }
+    }
+
+    fn contains_missing_playlist(&self, playlist: &Playlist) -> bool {
+        for entry in playlist.contents.iter() {
+            if let PlaylistEntry::Playlist(uuid) = entry {
+                if !self.playlists.contains_key(uuid) {
+                    return true
+                }
+            }
+        }
+        false
+    }
+
+    fn contains_missing_song(&self, playlist: &Playlist) -> bool {
+        for entry in playlist.contents.iter() {
+            if let PlaylistEntry::Song(uuid) = entry {
+                if !self.songs.contains_key(uuid) {
+                    return true
+                }
+            }
+        }
+        false
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct Config {
+    #[serde(default)]
+    pub data_directory: String
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-use media_player::{AudioSource, MediaLibrary, Playlist, PlaylistEntry};
+use std::fs::File;
+use media_player::{Config, MediaLibrary};
 
 fn main() {
-    let mut mediaLibrary = MediaLibrary::load();
+    // If we can't find the user's base directories, we can't load the config.
+    let base_dirs = directories::BaseDirs::new().expect("Could not find user's BaseDirs");
+    let mut config_dir = base_dirs.config_dir().to_path_buf();
+    config_dir.push("Media Player");
+    config_dir.push("config.json");
+
+    let config_file = File::open(config_dir);
+    let mut config = if let Ok(file) = config_file {
+        // If we can't parse the config file, we don't want to exit instead of loading a default config file
+        serde_json::from_reader(file).expect("Could not parse config file")
+    } else {
+        // the config file does not exist or is unreadable so use the default config.
+        Config::default()
+    };
+
+    if config.data_directory.is_empty() {
+        // I'm not sure when this would ever fail in practice even though it theoretically could.'
+        let mut data_dir = base_dirs.data_dir().to_path_buf();
+        data_dir.push("Nested Playlist Media Player");
+        config.data_directory = data_dir.to_str().expect("Could not parse data directory as a string").to_owned();
+    }
+
+    let library = MediaLibrary::load(config);
 }
